e19381377a425e57189d2eeced0550d6b8d1091d
Scott White <whitews@gmail.com>
2018-10-08 10:13:39 -0400

    remove unused imports

---
+++ classifier/test.py
@@ -0,0 +1,18 @@
+from utils.data import create_generator_from_stash
+from keras.models import load_model
+from classifier.architecture import weighted_categorical_crossentropy
+
+gen = create_generator_from_stash('data/test_numpy', batch_size=200)
+x,y = next(gen)
+
+model = load_model('classifier/model_1.hdf5',
+                   custom_objects = {'loss':  weighted_categorical_crossentropy((1, 6, 6, 13, 13))})
+
+pred = model.predict(x)
+
+
+gen = create_generator_from_stash('data/test_numpy', batch_size=400)
+x,y = next(gen)
+
+
+pred2 = model.predict(x)
---
+++ classifier/xception_transfer.py
@@ -0,0 +1,73 @@
+from keras.callbacks import ModelCheckpoint
+from utils.data import create_generator_from_stash
+import matplotlib.pyplot as plt
+from classifier.architecture import build_model_double, build_model, build_model_scratch
+
+gen = create_generator_from_stash('data/train_numpy')
+
+checkpoint = ModelCheckpoint(
+    'classifier/model_1.hdf5',
+    monitor='loss',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=False,
+    mode='auto',
+    period=1
+)
+
+model = build_model_scratch()
+
+h = model.fit_generator(
+    gen,
+    steps_per_epoch=70,
+    epochs=10,
+    callbacks=[checkpoint]
+)
+
+print(h.history.keys())
+# summarize history for accuracy
+plt.plot(h.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(h.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+
+model2 = build_model_double('classifier/model_1.hdf5')
+
+checkpoint2 = ModelCheckpoint(
+    'classifier/model_2.hdf5',
+    monitor='acc',
+    verbose=1,
+    save_best_only=True,
+    save_weights_only=False,
+    mode='auto',
+    period=1
+)
+
+h2 = model.fit_generator(
+    gen,
+    steps_per_epoch=70,
+    epochs=10,
+    callbacks=[checkpoint2]
+)
+plt.plot(h2.history['acc'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+# summarize history for loss
+plt.plot(h2.history['loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
